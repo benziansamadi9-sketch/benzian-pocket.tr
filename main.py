297f0a2cdbcf0c9dc3427d6d5869956dd6d4206f
benziansamadi9-sketch <benziansamadi9@gmail.com>
2025-10-22 16:04:19 +0200

    Create main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Pocket Bot AI started successfully!")
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class PocketApp(App):
+    def build(self):
+        return Label(text="مرحباً بك في تطبيق التوقعات 🎯")
+
+if __name__ == "__main__":
+    PocketApp().run()
+  
